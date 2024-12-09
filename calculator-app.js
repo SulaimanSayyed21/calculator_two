ea76942d235ba5a112594fb5c2cdce8476d2dab9
SulaimanSayyed21 <sullisayyed21@outlook.com>
2024-12-09 20:55:09 +0000

    Initialised Node.js project and added calculator library

---
+++ calculator-app.js
@@ -0,0 +1,7 @@
+// calculator-app.js
+const calculator = require('./calculator');
+
+console.log("Addition:", calculator.add(5, 3));       // Should print 8
+console.log("Subtraction:", calculator.subtract(5, 3)); // Should print 2
+console.log("Multiplication:", calculator.multiply(5, 3)); // Should print 15
+console.log("Division:", calculator.divide(5, 3));     // Should print 1.666
