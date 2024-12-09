4072f67e901ade968976ec3d05d4ba187aec74d7
SulaimanSayyed21 <sullisayyed21@outlook.com>
2024-12-09 23:43:56 +0000

    Routes structure added for calculator functions without logic

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
-const express = require('express');
+const express = require("express");
+const calculator = require("./calculator");
 const app = express();
 const port = 3000;
+// Define routes
 
-// Define a simple route
-app.get('/', (req, res) => {
-    res.send('Server is running on localhost:3000');
+// Route to use the add function of calculator
+app.post("/add", (req, res) => {
+
 });
 
-// Start the server
+// Route to use the subtract function of calculator
+app.post("/subtract", (req, res) => {
+
+});
+
+// Route to use the multiply function of calculator
+app.post("/multiply", (req, res) => {
+
+});
+
+// Route to use the divide function of calculator
+app.post("/divide", (req, res) => {
+
+});
+
+//
+app.get('/', (req, res) => {
+res.send('Server is running on localhost:3000');
+
+});
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+console.log(`Server is running on http://localhost:${port}`);
 });
